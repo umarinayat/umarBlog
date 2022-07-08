0a622c28bd831d36fed4a2f9296456d5c8d8ab22
= <=>
2022-07-08 12:08:06 +0500

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <h1>Umar Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a
+            href="/create"
+            style={{
+              color: "white",
+              backgroundColor: "#f1356d",
+              borderRadius: "8px",
+            }}
+          >
+            New Blog
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
