40f022861a9cc8b71722375b6a7cbacae7c877c5
= <=>
2022-07-08 15:56:24 +0500

    custom hook useFetch

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,20 @@
 import { useState, useEffect } from "react";
 
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const {
+    data: blogs,
+    pending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {pending && <div>Loading....</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
---
+++ src/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [pending, setPending] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data for this resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setPending(false);
+        setError(err.message);
+      });
+  }, [url]);
+
+  return { data, pending, error };
+};
+
+export default useFetch;
